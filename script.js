091c14414d726f8a71066e027bf62cbd8ce83bf8
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-10-03 12:12:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
+const str=window.prompt("Enter the word: ");
 
 function palindrome(str){
-
+	let reverseWord=""
+	for(let i=str.length-1;i>=0;i--){
+		reverseWord+=str[i]
+	}
+	if(reverseWord==str){
+		return true
+	}
+	return false
 }
+
+alert(palindrome(str))
 module.exports = palindrome
